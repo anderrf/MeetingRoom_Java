0f9d6c3a2a06f3fba672725a8c50847abad7cd11
anderrf <anderson_rocha_farias@outlook.com>
2021-08-21 14:48:29 -0300

    Construction of Room model

--- src/main/java/com/dio/meetingroom/model/Room.java
+++ src/main/java/com/dio/meetingroom/model/Room.java
@@ -1,5 +1,30 @@
 package com.dio.meetingroom.model;
 
-public class Room {
+import lombok.*;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name="meetingRoom")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+    @Column(name = "startHour", nullable = false)
+    private LocalTime startHour;
+    @Column(name = "endHour", nullable = false)
+    private LocalTime endHour;
 }
